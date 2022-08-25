bcdbd85c8570491f4f4589c4908bcb413f7493c1
Magnus Lundmark <magnus@skysense.io>
2022-08-25 15:04:09 +0200

    changes

--- include/casport.h
+++ include/casport.h
@@ -1,31 +1,42 @@
 #ifndef INCLUDED_CASPORT_H
 #define INCLUDED_CASPORT_H
 
-#include <stdint.h>
-#include <stdio.h>
 #include <complex>
+#include <cmath>
+#include <list>
+
 typedef std::complex<double> cxd_t;
 inline constexpr double C0 = 299792458.0;
 inline constexpr double C0_REC = 3.33564095198152049575;
 
-
 namespace casport {
-class element	{
+typedef enum emt { TRL = 0, CAP = 1, IND = 2, RES = 3, OCS = 4, SCS = 5} element_t;
+typedef enum mnt { SHUNT = 0, SERIES = 1} mount_t;
+
+class element {
 	public:
-        typedef enum emt { TRL = 0, CAP = 1, IND = 2, RES = 3, OCS = 4, SCS = 5} element_t;
-    	typedef enum mnt { SERIES = 0, SHUNT = 1} mount_t;
-		element();
+		element(const casport::element_t e,const casport::mount_t m, const double v);
+		element(const double l, const double z0);
+		void lmult(cxd_t *m);
 		~element();
+	private:
+		element_t m_element;
+		mount_t m_mount;
+		cxd_t m_abcd[3]; // a11 a12
+		cxd_t m_impedance;
+		cxd_t m_admittance;
 };
 
 class circuit {
 public:
 	circuit();
 	~circuit();
+	cxd_t impedance();
+	void push_back(const element e);
+private:
+	std::list<element> m_circuit;
 };
 
 }
-
-
 
 #endif //INCLUDED_CASPORT_H
--- src/casport.cc
+++ src/casport.cc
@@ -2,7 +2,21 @@
 
 namespace casport{
 
-element::element(){
+element::element(const casport::element_t e, const mount_t m, const double v) {
+	switch (e) {
+	case TRL:
+	break;
+	case CAP:
+	break;
+	case IND:
+	break;
+	case RES:
+	break;
+	case OCS:
+	break;
+	case SCS:
+	break;		
+		}
 }
 
 element::~element(){
